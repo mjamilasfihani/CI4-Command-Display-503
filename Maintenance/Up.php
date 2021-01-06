f7ecc131bc36dc13f644e2294a487e444adfec10
Mohammad Jamil Asfihani <51300528+mjamilasfihani@users.noreply.github.com>
2021-01-06 14:03:16 +0700

    Update Up.php

--- Maintenance/Up.php
+++ Maintenance/Up.php
@@ -5,29 +5,61 @@ use CodeIgniter\CLI\CLI;
 
 class Up extends BaseCommand
 {
-    protected $group       = 'Maintenance';
-    protected $name        = 'server:up';
+
+    /**
+     * The group the command is lumped under
+     * when listing commands.
+     *
+     * @var string
+     */
+    protected $group = 'Maintenance';
+
+    /**
+     * The Command's name
+     *
+     * @var string
+     */
+    protected $name = 'server:up';
+
+    /**
+     * the Command's short description
+     *
+     * @var string
+     */
     protected $description = 'Deactivate the maintenance mode.';
 
+    //--------------------------------------------------------------------
+
+    /**
+     * Ensures that all server:up have been run.
+     *
+     * @param array $params
+     */
     public function run(array $params)
     {
-        // Location 503.php file
-        $path = WRITEPATH . 'uploads/503.php';
+        // Location the file
+        $path = WRITEPATH . 'uploads/';
 
-        // We need to check the file first
-        if (file_exists($path) === false)
+        // We need to check the files first
+        if (file_exists($path . '503.php') && file_exists($path . '503.html'))
+        {
+            // Deleting the files
+            if (unlink($path . '503.php') && unlink($path . '503.html'))
+            {
+                CLI::write('Maintenance mode deactivate!', 'green');
+                return;
+            }
+            else
+            {
+                CLI::error('File 503.php or 503.html can\'t be deleted!');
+                return;
+            }
+        }
+        else
         {
             CLI::error('Maintenance mode has deactivated!');
             return;
         }
-
-        // Delete 503.php file
-        if (! unlink($path))
-        {
-            CLI::error('File 503.php can\'t be deleted!');
-            return;
-        }
-
-        CLI::write('Maintenance mode deactivate!', 'green');
     }
+
 }
